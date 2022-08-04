e7cd17d9403cd6c816135fc39bc556a71a6a6e93
Cesar Augusto Moreno <30708891+cesarmorenocol@users.noreply.github.com>
2022-08-04 11:25:48 -0500

    Update Program.cs

--- src/GDrivePoC/Program.cs
+++ src/GDrivePoC/Program.cs
@@ -13,7 +13,7 @@ namespace GDrivePoC
             // Upload a file to root file:
             var helper = new GoogleDriveHelper();
             var progress = new Progress<long>(pct => Console.WriteLine($"Progress %{pct}"));
-            Task.Run(() => helper.UploadFileAsync("C:\\Temp\\SampleFiles\\SCAIN Intranet Arq.pptx", progress));
+            Task.Run(() => helper.UploadFileAsync("C:\\Temp\\SampleFiles\\Presentaion.pptx", progress));
             Console.Read();
         }
     }
